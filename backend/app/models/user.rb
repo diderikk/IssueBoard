c8320296758edd3d60e7ad3256f7cad372264328
Diderik Kramer <diderikk@stud.ntnu.no>
2021-06-21 14:31:36 +0200

    Remove folders

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-user = User.create(email: "diderikk@gmail.com", password: "hash")
+users = User.create([{email: "diderikk@gmail.com", password: "hash123"}, {email: "did@gmail.com", password: "hash1234"}])
