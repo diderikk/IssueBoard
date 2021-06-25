246aae751037960209273f829b9d4106f31781a9
Diderik Kramer <diderikk@stud.ntnu.no>
2021-06-25 19:05:09 +0200

    Add backend tests

--- backend/test/models/issue_board_test.rb
+++ backend/test/models/issue_board_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class IssueBoardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save issue board without user or group' do
+    issue_board = IssueBoard.new(name: "IssueBoard")
+    assert_not issue_board.save, "Issue board not saved"
+  end
+
+  test "should not save issue board with both user and group" do
+
+    issue_board = IssueBoard.new(name: "TestBoard", user_id: users(:one).id, group_id: groups(:group1).id)
+    assert_not issue_board.save, "Issue board not saved"
+  end
+
+  test "should save issue board" do
+    issue_board = IssueBoard.new(name: "Test", group_id: groups(:group1).id)
+    assert issue_board.save, "Issue board saved"
+  end
+
 end
