7eb307c59aad1116a2880a815c16f40a8897acb1
Diderik Kramer <diderikk@stud.ntnu.no>
2021-06-25 19:59:37 +0200

    Add CI

--- backend/test/models/issue_test.rb
+++ backend/test/models/issue_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class IssueTest < ActiveSupport::TestCase
   test "should not save issue when wrong format due date" do
     issue = Issue.new(issue_id: 200, title: "Title", due_date: "2012-12-12", issue_label_id: issue_labels(:issue_label1).id)
-    assert issue.save, "Issue not saved"
+    assert_not issue.save, "Issue not saved"
   end
 end
